eb1e75750baa14449b07a7eb91eec14791013767
HerschelW <herschelw@gmail.com>
2022-12-15 12:57:24 -0600

    updating day1Fix

--- day1Fix/src/main.rs
+++ day1Fix/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // turn input.txt into an array
+    let input = include_str!("../input.txt");
+    let input: Vec<&str> = input.split_whitespace().collect();
+    // turn array into a vector of integers
+    let mut input: Vec<i32> = input.iter().map(|x| x.parse().unwrap()).collect();
+    // sort vector
+    input.sort();
+    // assign the largest integer to a variable 'largest'
+    let largest = input[input.len() - 1];
+    // print largest
+    println!("{:?}", largest);
 }
