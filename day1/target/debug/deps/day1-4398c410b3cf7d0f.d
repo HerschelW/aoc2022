bcdfbffc6825b98b2880dc941f9722baddc26b2d
Kent Worthington <theaceco@protonmail.com>
2022-12-15 13:07:51 -0600

    Delete day1/target directory

--- day1/target/debug/deps/day1-4398c410b3cf7d0f.d
+++
@@ -1,6 +0,0 @@
-/home/scoundrel/projects/advent_of_code/day1/target/debug/deps/day1-4398c410b3cf7d0f.rmeta: src/main.rs src/../input.txt
-
-/home/scoundrel/projects/advent_of_code/day1/target/debug/deps/day1-4398c410b3cf7d0f.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
--- day1/target/debug/deps/day1Fix-0282992392c51ec9.d
+++
@@ -1,8 +0,0 @@
-/home/scoundrel/projects/advent_of_code/day1/target/debug/deps/day1-0282992392c51ec9.rmeta: src/main.rs src/../input.txt
-
-/home/scoundrel/projects/advent_of_code/day1/target/debug/deps/day1-0282992392c51ec9.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
-
-# env-dep:CLIPPY_ARGS=
