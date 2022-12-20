dde1a20e1b15f2e88a20961f0f1685edf2d1a41d
HerschelW <herschelw@gmail.com>
2022-12-20 13:28:14 -0600

    read the problem backwards. DOH! Day2 Part1 complete. Refactored comparison function

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -10,33 +10,54 @@ fn main() {
 }
 
 fn rochambeau() {
+    // Read input file
     let input = std::fs::read_to_string("src/input.txt").unwrap();
+
+    // split lines into vector
     let lines: Vec<&str> = input.lines().collect();
+
+    // compare a to b in lines vector
+    // if a == b, add 3 to score
+    // if a == rock and b == scissors, add 6 to score
+    // if a == paper and b == rock, add 6 to score
+    // if a == scissors and b == paper, add 6 to score
+    // if a == rock and b == paper, add 0 to score
+    // if a == paper and b == scissors, add 0 to score
+    // if a == scissors and b == rock, add 0 to score
+
+    // create score variable
     let mut score = 0;
+    let mut round = 1;
+    // loop through lines vector
     for i in &lines {
+        // print i
+        println!("\nRound {}", round);
+        // get a and b
         let a = i.chars().next().unwrap();
+        // print a
+        println!("a: {}", a);
         let b = i.chars().nth(2).unwrap();
-        match a {
-            'A' => match b {
-                'X' => score += 4,
-                'Y' => score += 1,
-                'Z' => score += 7,
-                _ => (),
-            },
-            'B' => match b {
-                'X' => score += 8,
-                'Y' => score += 5,
-                'Z' => score += 2,
-                _ => (),
-            },
-            'C' => match b {
-                'X' => score += 3,
-                'Y' => score += 9,
-                'Z' => score += 6,
-                _ => (),
-            },
-            _ => (),
+        // print b
+        println!("b: {}", b);
+
+        // increment round
+        round += 1;
+        // match b and a as string
+        match (b, a) {
+            ('X', 'B') => score += 1,
+            ('Y', 'C') => score += 2,
+            ('Z', 'A') => score += 3,
+            ('X', 'A') => score += 4,
+            ('Y', 'B') => score += 5,
+            ('Z', 'C') => score += 6,
+            ('X', 'C') => score += 7,
+            ('Y', 'A') => score += 8,
+            ('Z', 'B') => score += 9,
+            _ => println!("Error"),
         }
+        // print score
+        println!("New score: {}", score);
     }
-    println!("score: {}", score);
+    // print lines length
+    println!("lines length: {}", lines.len());
 }
