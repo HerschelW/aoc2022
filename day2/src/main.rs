c58d39f3b586c80ca647b969a9cd31dbee298302
HerschelW <herschelw@gmail.com>
2022-12-20 15:50:44 -0600

    fixing errors - attempt number 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -15,6 +15,8 @@ fn rochambeau() {
 
     // split lines into vector
     let lines: Vec<&str> = input.lines().collect();
+    // print lines
+    println!("lines: {:?}", lines);
 
     // compare a to b in lines vector
     // if a == b, add 3 to score
@@ -27,21 +29,13 @@ fn rochambeau() {
 
     // create score variable
     let mut score = 0;
-    let mut round = 1;
+    let mut part2_score = 0;
+    let mut choices: Vec<String> = Vec::new();
     // loop through lines vector
     for i in &lines {
-        // print i
-        println!("\nRound {}", round);
         // get a and b
         let a = i.chars().next().unwrap();
-        // print a
-        println!("a: {}", a);
         let b = i.chars().nth(2).unwrap();
-        // print b
-        println!("b: {}", b);
-
-        // increment round
-        round += 1;
         // match b and a as string
         match (b, a) {
             ('X', 'B') => score += 1,
@@ -56,8 +50,66 @@ fn rochambeau() {
             _ => println!("Error"),
         }
         // print score
-        println!("New score: {}", score);
+        // println!("New score: {}", score);
+
+        // part 2
+        // create empty char variable
+        let mut choice = ' ';
+        // create empty string variable
+        match b {
+            'X' => match a {
+                'A' => choice = 'Z',
+                'B' => choice = 'X',
+                'C' => choice = 'Y',
+                _ => (),
+            },
+            'Y' => match a {
+                'A' => choice = 'X',
+                'B' => choice = 'Y',
+                'C' => choice = 'Z',
+                _ => (),
+            },
+            'Z' => match a {
+                'A' => choice = 'Y',
+                'B' => choice = 'Z',
+                'C' => choice = 'X',
+                _ => (),
+            },
+            _ => println!("Error"),
+        }
+
+        // make single str from a and choice
+        let choice_str = format!("{} {}", a, choice);
+
+        // push choice_str to choices vector
+        choices.push(choice_str)
     }
-    // print lines length
-    println!("lines length: {}", lines.len());
+
+    // loop through choices vector
+    for i in &choices {
+        // get a and b
+        let a = i.chars().next().unwrap();
+        // print a
+        println!("a: {}", a);
+        let b = i.chars().nth(2).unwrap();
+        // print b
+        println!("b: {}", b);
+        // match b and a as string
+        match (b, a) {
+            ('X', 'B') => part2_score += 1,
+            ('Y', 'C') => part2_score += 2,
+            ('Z', 'A') => part2_score += 3,
+            ('X', 'A') => part2_score += 4,
+            ('Y', 'B') => part2_score += 5,
+            ('Z', 'C') => part2_score += 6,
+            ('X', 'C') => part2_score += 7,
+            ('Y', 'A') => part2_score += 8,
+            ('Z', 'B') => part2_score += 9,
+            _ => println!("Error"),
+        }
+    }
+
+    // print score
+    println!("Final score: {}", score);
+    println!("Part 2 score: {}", part2_score);
 }
