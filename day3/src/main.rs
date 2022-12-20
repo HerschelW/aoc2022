8e1a8d108adecd4f3f0ad889716bc9c496c8f5df
HerschelW <herschelw@gmail.com>
2022-12-20 17:41:02 -0600

    took a wrong turn somewhere

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,41 +4,24 @@ fn main() {
 
 fn solve_day_3() {
     // parse input.txt
-    let input = include_str!("sample_input.txt");
+    let input = include_str!("input.txt");
     let lines = input.lines();
-    // print first 5 lines
-    let mut _matching_item = ' ';
     // empty vector
     let mut matching_items: Vec<char> = Vec::new();
     for line in lines {
-        // print length of line
-        println!("{}: {}", line.len(), line);
         // split line into two halves
         let (left, right) = line.split_at(line.len() / 2);
-        // print left and right halves
-        println!("{} {}", left, right);
-        // find matching letters in halves
-        let mut matches = 0;
 
         for l in left.chars() {
             for r in right.chars() {
                 if l == r {
-                    matches += 1;
-                    _matching_item = l;
-                    matching_items.push(_matching_item);
+                    matching_items.push(l);
                 }
             }
         }
-        // print number of matches
-        println!("{} matches", matches);
     }
-    // remove duplicate items
-    matching_items.sort();
-    matching_items.dedup();
-    // print number of unique matches
-    println!("{} unique matches", matching_items.len());
 
-    // print matching_items
+    // print matching items
     println!("{:?}", matching_items);
 
     // assign values 1 through 26 to each lowercase letter of the alphabet
@@ -50,8 +33,6 @@ fn solve_day_3() {
     for i in 1..27 {
         alphabet.push((i + 64) as u8 as char);
     }
-    // print alphabet
-    println!("{:?}", alphabet);
 
     // find value for matching items
     let mut matching_values: Vec<i32> = Vec::new();
