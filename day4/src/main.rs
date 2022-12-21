5e6d7176307363102fb615a1f16046a1b84d21f1
HerschelW <herschelw@gmail.com>
2022-12-21 17:24:06 -0600

    day 4 initial setup

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,20 @@
+// use Lines
+use std::str::Lines;
+
 fn main() {
-    println!("Hello, world!");
+    // read input file
+    let input = include_str!("input.txt");
+    // split input into lines
+    let lines = input.lines();
+
+    // solve day 4 part 1
+    solve_day_4_part_1(lines);
+}
+
+// create part 1 function with Lines as parameter
+fn solve_day_4_part_1(lines: Lines) {
+    // print lines
+    for line in lines {
+        println!("{}", line);
+    }
 }
