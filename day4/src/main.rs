49405028368fb4e0147424575881a4fc6526ffea
HerschelW <herschelw@gmail.com>
2022-12-22 14:23:05 -0600

    function to spread lists added

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,7 +3,7 @@ use std::str::Lines;
 
 fn main() {
     // read input file
-    let input = include_str!("input.txt");
+    let input = include_str!("sample_input.txt");
     // split input into lines
     let lines = input.lines();
 
@@ -13,8 +13,25 @@ fn main() {
 
 // create part 1 function with Lines as parameter
 fn solve_day_4_part_1(lines: Lines) {
-    // print lines
+    // vector of vectors
+    let mut list_of_lists = Vec::new();
     for line in lines {
-        println!("{}", line);
+        // split lines at comma
+        let split_line = line.split(",");
+        for split in split_line {
+            // populate numerical list, first value is beginning, last value is ending
+            let numerical_list: Vec<i32> = split.split("-").map(|s| s.parse().unwrap()).collect();
+            // make new list that spreads numerical list[0]...numerical_list[1]
+            let mut new_list = Vec::new();
+            for i in numerical_list[0]..numerical_list[1] + 1 {
+                new_list.push(i);
+            }
+            // print new list
+            println!("{:?}", new_list);
+            // push new list to list of lists
+            list_of_lists.push(new_list);
+        }
     }
+    // print list of lists
+    println!("{:?}", list_of_lists);
 }
