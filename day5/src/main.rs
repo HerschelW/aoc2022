ce8becb5c8af90ad12ac38d0bd0620ef24bef777
HerschelW <herschelw@gmail.com>
2022-12-23 13:34:23 -0600

    starting day 5 AOC

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,23 @@
+// use fs
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // import input using fs
+    let input = fs::read_to_string("sample_input.txt").expect("Error reading input file");
+
+    // solve day 5
+    solve_day_5(input);
+}
+
+fn solve_day_5(input: String) {
+    // split into two lines at \n\n
+    let mut lines = input.split("\n\n");
+    // get first line
+    let crates = lines.next().unwrap();
+    // get second line
+    let instructions = lines.next().unwrap();
+
+    // print lines
+    println!("Line 1: {}", crates);
+    println!("Line 2: {}", instructions);
 }
