74457b68257946c33612a847d1d9725416b11a55
Kent Worthington <theaceco@protonmail.com>
2022-12-23 13:36:57 -0600

    Delete day5/target directory

--- day5/target/debug/day5.d
+++
@@ -1,1 +0,0 @@
-/home/scoundrel/projects/advent_of_code/day5/target/debug/day5: /home/scoundrel/projects/advent_of_code/day5/src/main.rs
--- day5/target/debug/deps/day5-059125cb504447a2.d
+++
@@ -1,5 +0,0 @@
-/home/scoundrel/projects/advent_of_code/day5/target/debug/deps/day5-059125cb504447a2: src/main.rs
-
-/home/scoundrel/projects/advent_of_code/day5/target/debug/deps/day5-059125cb504447a2.d: src/main.rs
-
-src/main.rs:
